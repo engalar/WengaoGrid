4dbcf088e5a1f4582b1a16ffb43c4f52c2a009dc
engalar <engalar@gmail.com>
2024-06-02 14:15:12 +0800

    chore: setup vite

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import vitePluginMendix from "@engalar/vite-plugin-mendix";
+import pkg from "./package.json";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        vitePluginMendix({
+            widgetName: pkg.widgetName,
+            widgetPackage: pkg.packagePath,
+            testProject: pkg.config.projectPath
+        })
+    ]
+    /* ,     build: {
+        lib: {
+            entry: "tests/seed/task/index.ts",
+            name: "SeedLib",
+            fileName: (format, entryName) => `SeedLib-${entryName}.${format}.js`
+        },
+        rollupOptions: {
+            output: {
+                format: "amd"
+            },
+            external: ["react", "react-dom", "react/jsx-runtime", "big.js"]
+        }
+    } */
+});
