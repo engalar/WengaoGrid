900d6779d02cb035d82831ea90667f424e8e5104
engalar <engalar@gmail.com>
2024-07-03 17:39:53 +0800

    more option demo

--- packages/wengaoVideoPlayer/src/components/HelloWorldSample.tsx
+++ packages/wengaoVideoPlayer/src/components/HelloWorldSample.tsx
@@ -12,7 +12,7 @@ export function HelloWorldSample({ }: HelloWorldSampleProps): ReactElement {
     useEffect(() => {
         const dp = new DPlayer({
             container: playerRef.current,
-            preload: 'none',
+            preload: 'auto',
             autoplay: false,
             theme: '#FADFA3',
             loop: true,
@@ -24,13 +24,30 @@ export function HelloWorldSample({ }: HelloWorldSampleProps): ReactElement {
             volume: 0.2,
             mutex: true,
             lang: 'zh-cn',
+            highlight: [{ text: 'highlight text', time: 600 }, { text: 'highlight text2', time: 1200 }],
             video: {
-                // url: 'file2?fileid=1',
-                url: 'https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4',
-                pic: 'https://i.loli.net/2019/06/06/5cf8c5d9c57b510947.png',
-                thumbnails: 'https://i.loli.net/2019/06/06/5cf8c5d9cec8510758.jpg'
+                quality: [
+                    {
+                        name: 'HD',
+                        url: 'demo.m3u8',
+                        type: 'hls',
+                    },
+                    {
+                        name: 'SD',
+                        url: 'https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4',
+                        type: 'normal',
+                    },
+                    {
+                        name: 'LD',
+                        url: 'file2?fileid=1',
+                    }
+                ], defaultQuality: 1,
+                // url: 'http://localhost:9001/api/v1/download-shared-object/aHR0cDovLzEyNy4wLjAuMTo5MDAwL3Rlc3QvMjAyNC0wNC0xOCUyMDA5LTA1LTMyLm1wND9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPURBRVJVSFU3VkdDN0FBUEZSS0JDJTJGMjAyNDA3MDMlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwNzAzVDA5MDc1NVomWC1BbXotRXhwaXJlcz00MzE5OSZYLUFtei1TZWN1cml0eS1Ub2tlbj1leUpoYkdjaU9pSklVelV4TWlJc0luUjVjQ0k2SWtwWFZDSjkuZXlKaFkyTmxjM05MWlhraU9pSkVRVVZTVlVoVk4xWkhRemRCUVZCR1VrdENReUlzSW1WNGNDSTZNVGN5TURBME1ETTNNaXdpY0dGeVpXNTBJam9pVWs5UFZGVlRSVklpZlEuV3RZd2hlRzdpeWZabUE3Q0pZYnU1UlZhdXdrYWNabkY5eDRmUjg4Tm9WQm5ES2FHR0xSSXJYWlhsUTFQTmhfVmo0MDJBek05ZzdNbDVqWnJWQ20xSlEmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JnZlcnNpb25JZD1udWxsJlgtQW16LVNpZ25hdHVyZT04NmM2M2E5MzAzMDgwOTQ4YzhmODc0ZTBlOTZlM2I2OTA1Zjg5YzQ0OWJlMjk3MWE1ZjEyYTVjYWY3YjM1OWM5',
+                // url: 'https://api.dogecloud.com/player/get.mp4?vcode=5ac682e6f8231991&userId=17&ext=.mp4',
+                // pic: 'https://i.loli.net/2019/06/06/5cf8c5d9c57b510947.png',
+                // thumbnails: 'https://i.loli.net/2019/06/06/5cf8c5d9cec8510758.jpg'
             },
-            subtitle: {
+            /* subtitle: {
                 url: [
                     {
                         url: 'https://s-sh-17-dplayercdn.oss.dogecdn.com/hikarunara.vtt',
@@ -65,7 +82,7 @@ export function HelloWorldSample({ }: HelloWorldSampleProps): ReactElement {
                     text: 'custom contextmenu',
                     link: 'https://github.com/MoePlayer/DPlayer'
                 }
-            ]
+            ] */
         });
 
         const events: DPlayerEvents[] = [
